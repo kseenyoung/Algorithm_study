59c0f4c0f08807e7bdf7367858697636333c0666
kseenyoung <kseenyoung@gmail.com>
2024-02-21 20:48:28 +0900

    [BJ] 12919 A와B 2

--- S12919_A와B2.java
+++ S12919_A와B2.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class S12919_A와B2 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
@@ -10,32 +10,34 @@ public class S12919_A와B2 {
         PriorityQueue<String> que = new PriorityQueue();
 
         String S = br.readLine(); // S
-        que.offer(S);
         String T = br.readLine();  // T
-        int Tlength = T.length();
+        que.offer(T);
+        int Slength = S.length();
         String A = null, B = null;
 
-        while(!que.isEmpty()){
+        while (!que.isEmpty()) {
             String curr = que.poll();
-            if(curr.equals(S)){
+            if (curr.equals(S)) {
                 System.out.println(1);
                 return;
             }
+            String temp = null;
 
-            sb.setLength(0);
-            A = sb.append(curr).append("A").toString();
-            sb.setLength(0);
-            B = sb.append(curr).append("B").reverse().toString();
-
-            sb.setLength(0);
-            if(curr.length()+1 == Tlength){
-                if(A.equals(T) || B.equals(T)){
+            if (curr.length() > 1 && curr.charAt(curr.length() - 1) == 'A') {
+                temp = removeLastString(curr);
+                if (temp.equals(S)) {
                     System.out.println(1);
                     return;
                 }
-            } else if(curr.length()+1 < Tlength){
-                que.offer(A);
-                que.offer(B);
+                que.offer(temp);
+            }
+            if (curr.length() > 1 && curr.charAt(0) == 'B') {
+                temp = removeAndReverseString(curr);
+                if (temp.equals(S)) {
+                    System.out.println(1);
+                    return;
+                }
+                que.offer(temp);
             }
 
         }
@@ -43,10 +45,19 @@ public class S12919_A와B2 {
 
     }
 
-    public static String removeLastString(String str){
-        if(str == null || str.isEmpty())
+    public static String removeLastString(String str) {
+        if (str == null || str.isEmpty()) {
             return str;
+        }
         StringBuilder sb = new StringBuilder(str);
-//        return sb.deleteCharAt(sb.length() -1);
+        return sb.deleteCharAt(sb.length() - 1).toString();
+    }
+
+    public static String removeAndReverseString(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        StringBuilder sb = new StringBuilder(str);
+        return sb.deleteCharAt(0).reverse().toString();
     }
 }
