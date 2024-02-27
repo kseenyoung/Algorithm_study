0c9ef3410d9b3e922e1b6b8e615f72b6f1c2614e
김다나 <kdn1030@naver.com>
2024-02-28 00:11:10 +0900

    [Fix] 컨베이어 벨트 위의 로봇

--- Week04/BOJ_20055_컨베이어 벨트 위의 로봇.py
+++ Week04/BOJ_20055_컨베이어 벨트 위의 로봇.py
@@ -5,21 +5,37 @@ input = sys.stdin.readline
 N, K = tuple(map(int, input().split()))
 belt = deque(map(int, input().split()))
 robot = deque([0]*N)
-count = 0
-print(robot)
-# 초기 상태
-robot[0] = 1  # 로봇이 올리는 위치에 올라가고
-belt[0] -= 1  # 1번째 위치의 내구도는 하나 내려감.
-if not belt[0]:  # 만약 0 이 되었다면 count 1 올려줌.
-    count += 1
+count = belt.count(0)
+ans = 0
 
 while count < K:
-    belt.appendleft(belt.pop()) # 벨트 한 칸 회전함.
+    ans += 1
+    belt.appendleft(belt.pop())  # 벨트 한 칸 회전함.
     robot.pop()
-    robot.appendleft(0) # 로봇도 한 칸 같이 회전.
+    robot.appendleft(0)  # 로봇도 한 칸 같이 회전.
+    if robot[N-1] == 1:  # 로봇이 내리는 위치일 때 즉시 내림
+        robot[N-1] = 0
 
-    for i in range(N-2, -1, -1):
-        if belt[i+1] and not robot[i+1]:
-            if i == N-2:
-                belt[i+1] -= 1
-                robot[i]
+    for i in range(N-2, 0, -1):
+        if robot[i] == 1:
+            if belt[i+1] >= 1 and robot[i+1] == 0:  # belt 의 내구도가 1 이상이고 로봇이 없을 때.
+                if i == N-2:   # 내리는 위치에 로봇을 놓을 수 있을 때.
+                    belt[i+1] -= 1
+                    if belt[i+1] == 0:  #  벨트의 내구도가 0 이 되면 count + 1
+                        count += 1
+                    robot[i], robot[i+1] = 0, 0
+                else:
+                    belt[i+1] -= 1   # 옆으로 한 칸 이동할 때.
+                    if belt[i+1] == 0:
+                        count += 1
+                    robot[i] = 0
+                    robot[i+1] = 1
+
+    if belt[0] >= 1:
+        robot[0] = 1
+        belt[0] -= 1
+        if belt[0] == 0:
+            count += 1
+
+print(ans)
+
