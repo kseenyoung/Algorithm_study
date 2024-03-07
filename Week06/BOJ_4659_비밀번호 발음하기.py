bab90987841a5946f5a159577b9e9b3c92552f5d
dana <kdn1030@naver.com>
2024-03-07 09:40:59 +0900

    [Add] 비밀번호 발음하기

---
+++ Week06/BOJ_4659_비밀번호 발음하기.py
@@ -0,0 +1,38 @@
+def isVowel(c):
+    if c in ('a', 'e', 'i', 'o', 'u'):
+        return True
+    return False
+
+
+def func(pw):
+    if 'a' not in pw and 'e' not in pw and 'i' not in pw and 'o' not in pw and 'u' not in pw:
+        print("<{}> is not acceptable.".format(''.join(pw)))
+        return
+
+    consonantCount = 0
+    vowelCount = 0
+
+    for i in range(len(pw) - 2):
+        if isVowel(pw[i]) and isVowel(pw[i + 1]) and isVowel(pw[i + 2]):
+            print("<{}> is not acceptable.".format(''.join(pw)))
+            return
+
+        if not isVowel(pw[i]) and not isVowel(pw[i + 1]) and not isVowel(pw[i + 2]):
+            print("<{}> is not acceptable.".format(''.join(pw)))
+            return
+
+    for i in range(len(pw) - 1):
+        if pw[i] == pw[i + 1]:
+            if pw[i] not in ('e', 'o'):
+                print("<{}> is not acceptable.".format(''.join(pw)))
+                return
+
+    print("<{}> is acceptable.".format(''.join(pw)))
+    return
+
+while 1:
+    pw = list(input())
+    flag = 0
+    if pw == ['e', 'n', 'd']:
+        exit(0)
+    func(pw)
