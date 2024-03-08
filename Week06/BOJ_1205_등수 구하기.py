4f0dfdb5841e2df84dd9ab0333a36ff9d84c02e6
dana <kdn1030@naver.com>
2024-03-08 11:01:54 +0900

    [Add] 등수 구하기

---
+++ Week06/BOJ_1205_등수 구하기.py
@@ -0,0 +1,18 @@
+N, newScore, P = map(int, input().split())
+grade = 1
+if N > 0:
+    score = list(map(int, input().split()))
+
+    if P == N:
+        if score[N-1] >= newScore:
+            print(-1)
+            exit(0)
+
+    for i in range(len(score)):
+        if newScore < score[i]:
+            grade += 1
+        else:
+            print(grade)
+            exit(0)
+else:
+    print(grade)
