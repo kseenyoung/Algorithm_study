8d7aea80af87a133a7eac777e2054d8b25d7ead0
dana <kdn1030@naver.com>
2024-03-20 17:47:05 +0900

    [Fix] 크로스 컨트리

--- Week08/BOJ_9017_크로스컨트리.py
+++ Week08/BOJ_9017_크로스컨트리.py
@@ -7,7 +7,8 @@ for _ in range(T):
     N = int(input())
     data = list(map(int, input().split()))
     arr = defaultdict(int)
-    team = [[0, i] for i in range(200)]
+    # 팀 번호, 팀 점수, 팀의 인원, 팀의 팀의 5번째 선수의 점수.
+    team = [[i, 0, 0, 0] for i in range(200)]
 
     for d in data:
         arr[d] += 1
@@ -16,7 +17,10 @@ for _ in range(T):
         if arr[d] < 6:
             continue
         else:
-            team[d-1][0] += score
+            team[d-1][1] += score
+            team[d-1][2] += 1
+            if team[d-1][2] == 5:
+                team[d-1][3] = score
         score += 1
 
-    print(team)
+
